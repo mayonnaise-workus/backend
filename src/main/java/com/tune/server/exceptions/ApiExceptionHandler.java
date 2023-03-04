e36830c8e476079c20b9213224fd6de70b149c49
BEMELON <hgdkim2@naver.com>
2023-03-04 19:36:45 +0900

    fix: Handler에 Exception이 등록되지 않아 일정하지 않은 에러가 발생하고 있는 내용 수정

--- src/main/java/com/tune/server/exceptions/ApiExceptionHandler.java
+++ src/main/java/com/tune/server/exceptions/ApiExceptionHandler.java
@@ -2,6 +2,8 @@ package com.tune.server.exceptions;
 
 import com.tune.server.exceptions.login.InvalidTokenException;
 import com.tune.server.exceptions.login.KakaoServerException;
+import com.tune.server.exceptions.member.InvalidRequestException;
+import com.tune.server.exceptions.member.MemberNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,4 +13,20 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleMemberNotFoundException(MemberNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiErrorResponse.builder()
+                .code("MEMBER_NOT_FOUND")
+                .message(e.getMessage())
+                .build());
+    }
+
+    @ExceptionHandler(InvalidRequestException.class)
+    public ResponseEntity<ApiErrorResponse> handleInvalidRequestException(InvalidRequestException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiErrorResponse.builder()
+                .code("INVALID_REQUEST")
+                .message(e.getMessage())
+                .build());
+    }
 }
